6236c09f9203838feed5203d3059b0b7e7030716
bayezsyka <farrosy6@gmail.com>
2025-12-05 02:01:09 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -2,18 +2,14 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:3000'),
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://frontend-domain-mu.com', // ganti dengan domain Next.js kalau sudah ada
     ],
 
     'allowed_origins_patterns' => [],
@@ -25,5 +21,5 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,
-
 ];
+
